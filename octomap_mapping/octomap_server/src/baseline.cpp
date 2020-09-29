23192d1564fc43a24b01dfa0a91d1c5497c3b821
klyu-ironman <klyu@vt.edu>
2020-09-29 13:21:28 -0400

    creating baseline node

---
+++ octomap_mapping/octomap_server/src/baseline.cpp
@@ -0,0 +1,59 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <geometry_msgs/Quaternion.h>
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <ros/ros.h>
+#include <pcl/octree/octree_search.h>
+#include <octomap_msgs/Octomap.h>
+#include <octomap_msgs/conversions.h>
+#include <octomap/ColorOcTree.h>
+#include <pcl/io/pcd_io.h>
+#include "../include/octomap_server/kevin_functions.h"
+#include <geometry_msgs/Pose.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <hector_moveit_navigation/NavigationAction.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <gtsp/Tour.h>
+#include <gtsp/GTSPData.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <math.h>
+#include <string>
+#include <list>
+#include <set>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
+
+#define MAX_TOUR_SIZE 25
+
+
+// global variables
+
+
+int main(int argc, char** argv){
+// initializing ROS everything
+  ros::init(argc, argv, "baseline");
+  ros::NodeHandle n;
+  ros::Rate r(0.05); // less than 1 is slower
+
+
+// problem is that GLNS will crash if empty set
+  while (ros::ok()){
+    ros::spinOnce();
+
+  }
+}
