0581699c62c3e865c7fdf76b2fcbb0aeddca5d13
Kevin Yu <klyu@vt.edu>
2021-03-01 08:47:52 -0500

    trying to crop images correctly

--- pub_images.py
+++ pub_images.py
@@ -7,6 +7,7 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from filelock import FileLock
+import numpy as np
 
 source_image_dir = '../deep_lab_v3_material_detection/predicted_masks/'
 pub = rospy.Publisher('image_mask', Image, queue_size = 10)
@@ -14,7 +15,7 @@ rospy.init_node('image_mask_node', anonymous = True)
 
 #for image_name in tqdm(os.listdir(source_image_dir)):
 #print(image_name)
-
+color = (0,0,0)
 image_path = source_image_dir + 'test.jpeg'
 lock = FileLock(image_path + '.lock')
 while True:
@@ -22,12 +23,21 @@ while True:
         rospy.loginfo("File Locked")
         print(image_path)
         image = cv2.imread(image_path)
-        width, heigh = image.shape[1], image.shape[0]
+        width, height = image.shape[1], image.shape[0]
         min_ = min(width,height)
         dim = [min_, min_]
+        ht, wd, cc = image.shape
+        ww = 640
+        hh = 480
+        result = np.full((hh,ww,cc),color,dtype = np.uint8)
+        xx = (ww - wd) // 2
+        yy = (hh - ht) // 2
+        result[yy:yy+ht, xx:xx+wd] = image
+
+
         bridge = CvBridge()
         # image_msg = bridge.cv2_to_imgmsg(image, "passthrough")
-        image_msg = bridge.cv2_to_imgmsg(image, 'bgr8')
+        image_msg = bridge.cv2_to_imgmsg(result, 'bgr8')
         image_msg.header.stamp = rospy.Time.now()
         pub.publish(image_msg)
         rospy.loginfo('Publishing images')
